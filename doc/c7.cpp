91a51a19621d282d044dfb47dac9d830304d2184
gels <408963516@qq.com>
2021-11-26 14:19:57 +0800

    study

---
+++ doc/c7.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+
+class Base
+{
+public:
+    Base();
+    virtual ~Base();
+};
+
+Base::Base()
+{
+    printf("===Base::Base===\n");
+}
+
+Base::~Base()
+{
+    printf("===Base::~Base===\n");
+}
+
+class Son : public Base
+{
+public:
+    Son();
+    ~Son();
+};
+
+Son::Son()
+{
+    printf("===Son::Son===\n");
+}
+
+Son::~Son()
+{
+    printf("===Son::~Son===\n");
+}
+
+int main(int argv, char **argc)
+{
+    Base *p = new Son(); //子类在构造时, 会自动调用基类的构造函数
+    delete p;
+    // 输出如下, 并没有调用子类的析构函数
+    // ===Base::Base===
+    // ===Son::Son===
+    // ===Base::~Base===
+
+    //umdh
+    return true;
+}
