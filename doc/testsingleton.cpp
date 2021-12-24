2ed736c945bba5827587d02baebe678eba9f6bfa
gels <408963516@qq.com>
2021-12-24 18:06:08 +0800

    commit some test code

---
+++ doc/testsingleton.cpp
@@ -0,0 +1,12 @@
+//
+// Created by gels on 2021/12/24.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+int main(int arg, char **args)
+{
+
+    return 0;
+}
---
+++ lib/base/Singleton.h
@@ -0,0 +1,16 @@
+//
+// Created by gels on 2021/12/24.
+//
+
+#ifndef OPENGE2_SINGLETON_H
+#define OPENGE2_SINGLETON_H
+
+
+
+class Singleton {
+
+};
+
+
+
+#endif //OPENGE2_SINGLETON_H
