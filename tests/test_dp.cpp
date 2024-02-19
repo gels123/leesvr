fcf21f2b056715af15a023c218c1532cc5eb7643
gels <408963516@qq.com>
2024-02-19 11:07:35 +0000

    daily tests

--- tests/test_dp.cpp
+++ tests/test_dp.cpp
@@ -9,11 +9,17 @@
 #include<vector>
 using namespace std;
 
-auto vec = *new vector<int>(1024, 0);
+typedef struct sitem {
+    int id;
+    int weight;
+    int worth;
+} sitem;
 
-int dp(int *pweight, int* pworth, bool* flags, int n, int maxweight) {
-    if (maxweight >= 1024) {
-        return -1;
+int dp(sitem *items, int n, int& maxweight, vector<int>& vec) {
+    int weight = 1;
+    int worth = 0;
+    for (int i=0; i<=n; ++i) {
+
     }
     for (int w = 1; w <= maxweight; ++w) {
         int maxval = 0;
@@ -24,6 +30,52 @@ int dp(int *pweight, int* pworth, bool* flags, int n, int maxweight) {
 }
 
 int main(int argc, char** argv) {
+    //一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 10 级的台阶总共有多少种跳法
+    //要跳10级台阶，2种情况：先跳到8级再跳2 或先跳9级再跳1，即f(10) = f(9) + f(8)，即f(n) = f(n-1) + f(n-2)
+    int nums[11] = {0};
+    nums[1] = 1;
+    nums[2] = 2;
+    nums[3] = nums[2] + nums[1];//3
+    nums[4] = nums[3] + nums[2];//3+2=5
+    for (int i=3;i<=10;++i) {
+        nums[i] = nums[i-1] + nums[i-2];
+    }
+    cout << "qinwa10=" << nums[10] << endl;
+
+    //给你一个整数数组 nums ，找到其中最长严格递增子序列的长度. nums = [10,9,2,5,3,7,101,18]  长度为4
+    //f(8) = max{f(7) + 1?, f(6) + 1?, ...}
+    int array[8] = {10,9,2,5,3,7,101,18};
+    int dp[8] = {0};
+    int dpmax[8] = {0};
+    dp[0] = 1;
+    dpmax[0] = array[0];
+    for (int i=1; i<8; ++i) {
+        for (int j=i-1; j>=0; --j) {
+            if (array[i] >= dpmax[j]) {
+                int v = dp[j] + 1;
+                if (v > dp[i]) {
+                    dpmax[j] = array[i];
+                    dp[i] = v;
+                }
+            } else if(dp[i] == 0) {
+                dpmax[j] = array[i];
+                dp[i] = dp[j];
+            }
+        }
+    }
+    cout << "paixu7=" << dp[7] << endl;
+
+
+    sitem items[5] = {0};
+    items[0] = {0, 2, 3};
+    items[1] = {0, 1, 3};
+    items[2] = {0, 3, 1};
+    items[3] = {0, 2, 1};
+    items[4] = {0, 2, 3};
+    int maxweight = 5;
+    auto vec = *new vector<int>(512, 0);
+    cout << "items length=" << sizeof (items)/sizeof (sitem) << " maxweight=" << maxweight << " vec length=" << vec.size() << endl;
+
 
     return 0;
 }
--- tests/test_unorderdmap.cpp
+++ tests/test_unorderdmap.cpp
@@ -17,5 +17,13 @@ int main(int argc, char **argv)
         printf("map cell key=%s val=%d\n", it.first.c_str(), it.second);
     };
     std::for_each(map.begin(), map.end(), fn);
+
+    std::unordered_map<std::string, int> map2;
+    map2.insert(std::make_pair("hello world", 1));
+    map2.insert(std::make_pair("hey world", 2));
+    std::for_each(map2.begin(), map2.end(), [](std::pair<std::string, int> it) {
+        std::cout << "first=" << it.first << "second=" << it.second << std::endl;
+    });
+
     return 0;
 }
