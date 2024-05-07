1b021aa33ce3ecfbaa77c22a9c427d08bfb22931
gels <408963516@qq.com>
2024-05-07 02:56:36 +0000

    小优化

--- build_protobuf.sh
+++ build_protobuf.sh
@@ -2,26 +2,23 @@
 set -e  # exit immediately on error
 set -x  # display all commands
 
-mkdir -p third_party
-mkdir -p third_party/protobuf
-dir=`pwd`/third_party
+mkdir -p 3rd
+mkdir -p 3rd/protobuf
+dir=`pwd`/3rd/protobuf
 cd ${dir}
 
-if [ ! -f protobuf/bin/protoc ]; then
+if [ ! -f bin/protoc ]; then
 	if [ ! -f protobuf-cpp-3.0.0.tar.gz ]; then
 	  wget https://github.com/google/protobuf/releases/download/v3.0.0/protobuf-cpp-3.0.0.tar.gz
 	fi
 
 	tar -zxvf protobuf-cpp-3.0.0.tar.gz
 	cd protobuf-3.0.0
-	./configure --prefix=${dir}/protobuf
+	./configure --prefix=${dir}
 	make -j2
 	make install
 	cd ..
 	rm -rf protobuf-3.0.0
-
 fi
-
-#make
 
 exit $?
